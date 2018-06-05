2a7d9af80aea4d222d041a15578456dc65229de2
AlyssaEdwards13 <edwardsalyssa83@gmail.com>
2018-06-05 21:39:52 +0000

    automatically backed up by learn

---
+++ late_lunch.rb
@@ -0,0 +1,14 @@
+# if a number is divisible by 3, puts "fizz"
+# if a number is divisible by 5, puts "buzz"
+# if a number is divisible by 5 && 3, puts "fizzbuzz"
+#if not divisible by 5 or 3, puts NIL
+
+def fizzbuzz (i)
+  if i % 3 
+  puts "fizz"
+  elsif i % 5
+  puts "buzz"
+elsif i % 5 && % 3
+  puts "fizzbuzz"
+  
+end 
